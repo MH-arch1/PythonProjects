85defc22ee75c98d82699b8abb9dad5188123c39
MH-arch1 <1067997@pdsb.net>
2025-03-18 08:59:10 -0400

    Update day-of_the_week.py

---
+++ 05_Day_of_the_week_activity/day-of_the_week.py
@@ -0,0 +1,18 @@
+day = input("Enter the day of the week: ")
+
+if day == "Monday":
+    print("Start your week with a workout!")
+elif day == "Tuesday":
+    print("It's a great day to read a book!")
+elif day == "Wednesday":
+    print("Mid-week movie night!")
+elif day == "Thursday":
+    print("Try a new recipe!")
+elif day == "Friday":
+    print("Relax and enjoy the weekend!")
+elif day == "Saturday":
+    print("Go for a hike!")
+elif day == "Sunday":
+    print("Prepare for the week ahead with some self-care.")
+else:
+    print("Invalid input. Please enter a valid day of the week.")
